06d848ff9ba0f88c00be79664945d2ee8f8dae68
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2015-11-26 20:23:35 +0100

    Add sitemap plugin

--- pelicanconf.py
+++ pelicanconf.py
@@ -36,3 +36,21 @@ STATIC_PATHS = ['extra/CNAME']
 EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
 
 THEME = "themes/simple"
+
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['sitemap']
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
+
